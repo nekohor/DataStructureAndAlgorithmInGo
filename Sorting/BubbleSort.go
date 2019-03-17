03ddc82e81a9140d7e85e10ae0c5efd1120e0b88
nekohor <nekohor@foxmail.com>
2019-03-17 11:46:25 +0800

    bubble over

--- Sorting/BubbleSort.go
+++ Sorting/BubbleSort.go
@@ -1,12 +1,16 @@
-package BubbleSort
+package sorting
 
+// BubbleSort Implementation
 func BubbleSort(arr []int) []int {
 	length := len(arr)
-	swapped := false
+
 	for i := 0; i < length; i++ {
+		swapped := false
 		for j := 0; j < length-i-1; j++ {
-			arr[j], arr[j+1] = arr[j+1], arr[j]
-			swapped = true
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			}
 		}
 
 		if !swapped {
